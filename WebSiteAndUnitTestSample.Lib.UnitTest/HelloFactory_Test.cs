07f3ee9f5ba1c671cb10fe3fd68111f28961398c
Sky Chang <skchang@microsoft.com>
2018-04-27 19:45:10 +0800

    加入專案檔案。

---
+++ WebSiteAndUnitTestSample.Lib.UnitTest/HelloFactory_Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebSiteAndUnitTestSample.Lib.UnitTest
+{
+    [TestClass]
+    public class HelloFactory_Test
+    {
+        [TestMethod]
+        public void GetHello_Test()
+        {
+            var f = new HelloFactory();
+            var result = f.GetHello();
+            Assert.AreEqual("Hello World", result);
+        }
+    }
+}
